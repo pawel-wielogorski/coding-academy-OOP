3c40635bf05a6ac626bee74f7871cc38f7679d76
pawel-wielogorski <pwielogorski71@gmail.com>
2022-07-07 23:19:15 +0200

    task 1

--- task2.py
+++ task2.py
@@ -2,7 +2,10 @@ import json
 import xml.etree.ElementTree as ET
 import requests
 
-cnumber="2964002"   # numer klienta dla którego chcemy pobrać liste rachunków
+f = open(r"input_data.txt", "r")
+data = f.read()
+table = data.split('\n')
+print(len(table))
 
 def json_request(cnumber):   # budujemy z numeru klienta request JSON
     cunbr_={"cunbr":cnumber}
@@ -11,10 +14,10 @@ def json_request(cnumber):   # budujemy z numeru klienta request JSON
     return customer_request_
 
 def extract_cunbr_from_json(json):   # kontrolnie pobieramy numer klienta z JSONa zeby sprawdzic czy działa
-    return json["customer_request"]["customer"]["cunbr"]
+    return str(json["customer_request"]["customer"]["cunbr"])
 
-def create_web_request(cunbr):
-    return "https://coding-academy.pl/customer/" + extract_cunbr_from_json(cunbr) # budujemy z jsona zapytanie do serwisu web
+def create_web_request(cnumber):
+    return "https://coding-academy.pl/customer/"+ extract_cunbr_from_json(json_request(cnumber)) # budujemy z jsona zapytanie do serwisu web
 
 def xml2acc_list(cust_request):   # na podstawie odpwiedzi XML tworzymy liste rachunków
     response = requests.get(create_web_request(cnumber))
@@ -31,13 +34,15 @@ def list2json(cust_number,lista_acc):   # na bazie numeru klienta i listy rachun
     customer_response_ = {"customer_response":customer_}
     json_string = json.dumps(customer_response_)
     return json_string
+f = open("json_requests.txt", "a")
+g = open("json_responses.txt", "a")
 
+for cnumber in table:
+    f.write(json.dumps(json_request(cnumber))+"\n") # zapis zapytania JSON
+    #print(extract_cunbr_from_json(json_request(cnumber))) # wydruk numeru klienta z zapytania JSON
+    #print(create_web_request(cnumber))   # wydruk zapytania do serwisu web
+    #print(xml2acc_list(create_web_request(cnumber)))   # wydruk listy rachunków otrzymanej w XML z web serwisu
+    g.write(list2json(cnumber,xml2acc_list(create_web_request(cnumber)))+"\n") #  zapis JSONa utworzonego z numeru klienta i listy rachunków
 
-print(json_request(cnumber)) # wydruk zapytania JSON
-print(extract_cunbr_from_json(json_request(cnumber))) # wydruk numeru klienta z zapytania JSON
-print(create_web_request(cnumber))   # wydruk zapytania do serwisu web
-print(xml2acc_list(create_web_request(cnumber)))   # wydruk listy rachunków otrzymanej w XML z web serwisu
-print(list2json(cnumber,xml2acc_list(create_web_request(cnumber)))) #  wydruk JSONa utworzonego z numeru klienta i listy rachunków
-
-
-
+f.close()
+g.close()
